518fc3599e05d94fcaa0c80dc8a4a9d6b1822ff3
deno-fo <d4308970@gmail.com>
2026-09-05 18:21:00 +0300

    Initial commit

---
+++ src/test/java/io/github/denofo/mobilee2e/ProjectSetupTest.java
@@ -0,0 +1,13 @@
+package io.github.denofo.mobilee2e;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProjectSetupTest {
+
+    @Test
+    void projectShouldRunJUnitTests() {
+        assertTrue(true);
+    }
+}
