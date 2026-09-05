e33596ad172068295134fe81de433be0a279a70f
deno-fo <d4308970@gmail.com>
2026-09-05 20:47:17 +0300

    Add Android create board page model

---
+++ src/test/java/io/github/denofo/mobilee2e/pages/android/CreateBoardPage.java
@@ -0,0 +1,45 @@
+package io.github.denofo.mobilee2e.pages.android;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+
+public final class CreateBoardPage extends AndroidBasePage {
+
+    private static final By BOARD_NAME = AppiumBy.id("com.trello:id/board_name");
+    private static final By WORKSPACE = AppiumBy.id("com.trello:id/org_spinner");
+    private static final By VISIBILITY = AppiumBy.id("com.trello:id/visibility_spinner");
+    private static final By BACKGROUND = AppiumBy.id("com.trello:id/background_row");
+    private static final By CREATE_BOARD = AppiumBy.id("com.trello:id/create_board_button");
+
+    public CreateBoardPage(AndroidDriver driver) {
+        super(driver);
+    }
+
+    public CreateBoardPage waitUntilLoaded() {
+        visible(BOARD_NAME);
+        visible(CREATE_BOARD);
+        return this;
+    }
+
+    public CreateBoardPage enterBoardName(String name) {
+        type(BOARD_NAME, name);
+        return this;
+    }
+
+    public void submit() {
+        click(CREATE_BOARD);
+    }
+
+    public boolean isWorkspaceSelectorVisible() {
+        return isVisible(WORKSPACE);
+    }
+
+    public boolean isVisibilitySelectorVisible() {
+        return isVisible(VISIBILITY);
+    }
+
+    public boolean isBackgroundSelectorVisible() {
+        return isVisible(BACKGROUND);
+    }
+}
