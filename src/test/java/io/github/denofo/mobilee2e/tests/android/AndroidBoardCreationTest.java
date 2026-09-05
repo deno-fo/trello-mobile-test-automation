2eeeef67b8f1ad072afcc06f3c8d39f8edb69d76
deno-fo <d4308970@gmail.com>
2026-09-05 21:30:50 +0300

    Clean up created Trello boards via API

--- src/test/java/io/github/denofo/mobilee2e/tests/android/AndroidBoardCreationTest.java
+++ src/test/java/io/github/denofo/mobilee2e/tests/android/AndroidBoardCreationTest.java
@@ -1,5 +1,6 @@
 package io.github.denofo.mobilee2e.tests.android;
 
+import io.github.denofo.mobilee2e.api.trello.TrelloBoardCleanup;
 import io.github.denofo.mobilee2e.junit.AndroidDeviceTest;
 import io.github.denofo.mobilee2e.pages.android.BoardPage;
 import io.github.denofo.mobilee2e.pages.android.BoardsPage;
@@ -17,22 +18,25 @@ class AndroidBoardCreationTest extends BaseAndroidTest {
         String boardName =
                 "e2e-board-" + Instant.now().toEpochMilli();
 
-        BoardPage boardPage = new BoardsPage(driver)
-                .waitUntilLoaded()
-                .openCreateBoard()
-                .enterBoardName(boardName)
-                .createBoard();
+        try (TrelloBoardCleanup ignored =
+                     new TrelloBoardCleanup(boardName)) {
+            BoardPage boardPage = new BoardsPage(driver)
+                    .waitUntilLoaded()
+                    .openCreateBoard()
+                    .enterBoardName(boardName)
+                    .createBoard();
 
-        assertAll(
-                () -> assertEquals(
-                        boardName,
-                        boardPage.boardTitle(),
-                        "Created board title does not match."
-                ),
-                () -> assertTrue(
-                        boardPage.isAddListVisible(),
-                        "Add list button is not visible on the created board."
-                )
-        );
+            assertAll(
+                    () -> assertEquals(
+                            boardName,
+                            boardPage.boardTitle(),
+                            "Created board title does not match."
+                    ),
+                    () -> assertTrue(
+                            boardPage.isAddListVisible(),
+                            "Add list button is not visible on the created board."
+                    )
+            );
+        }
     }
 }
