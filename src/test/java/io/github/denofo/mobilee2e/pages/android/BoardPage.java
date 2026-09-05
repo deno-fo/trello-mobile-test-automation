fbdbf5c4f05cc792828cffd2cef7338f97063707
deno-fo <d4308970@gmail.com>
2026-09-05 20:50:29 +0300

    Add Android board page model

---
+++ src/test/java/io/github/denofo/mobilee2e/pages/android/BoardPage.java
@@ -0,0 +1,32 @@
+package io.github.denofo.mobilee2e.pages.android;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+
+public final class BoardPage extends AndroidBasePage {
+
+    private static final By TOOLBAR_TITLE =
+            AppiumBy.id("com.trello:id/toolbar_title");
+
+    private static final By ADD_LIST =
+            AppiumBy.id("com.trello:id/add_list_button");
+
+    public BoardPage(AndroidDriver driver) {
+        super(driver);
+    }
+
+    public BoardPage waitUntilLoaded() {
+        visible(TOOLBAR_TITLE);
+        visible(ADD_LIST);
+        return this;
+    }
+
+    public String boardTitle() {
+        return visible(TOOLBAR_TITLE).getText();
+    }
+
+    public boolean isAddListVisible() {
+        return isVisible(ADD_LIST);
+    }
+}
