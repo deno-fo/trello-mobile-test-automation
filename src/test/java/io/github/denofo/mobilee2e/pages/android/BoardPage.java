16d9d086a21c421abb2903e5cb08dfddd645942f
deno-fo <d4308970@gmail.com>
2026-09-07 18:30:13 +0300

    Support creating and verifying Trello lists

--- src/test/java/io/github/denofo/mobilee2e/pages/android/BoardPage.java
+++ src/test/java/io/github/denofo/mobilee2e/pages/android/BoardPage.java
@@ -12,6 +12,9 @@ public final class BoardPage extends AndroidBasePage {
     private static final By ADD_LIST =
             AppiumBy.id("com.trello:id/add_list_button");
 
+    private static final By LIST_NAME =
+            AppiumBy.id("com.trello:id/list_name");
+
     public BoardPage(AndroidDriver driver) {
         super(driver);
     }
@@ -28,5 +31,19 @@ public final class BoardPage extends AndroidBasePage {
 
     public boolean isAddListVisible() {
         return isVisible(ADD_LIST);
+    }
+
+    public AddListPage openAddList() {
+        click(ADD_LIST);
+        return new AddListPage(driver).waitUntilLoaded();
+    }
+
+    public BoardPage waitForList(String listName) {
+        visibleWithText(LIST_NAME, listName);
+        return this;
+    }
+
+    public boolean isListVisible(String listName) {
+        return isVisibleWithText(LIST_NAME, listName);
     }
 }
